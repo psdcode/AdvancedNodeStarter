ff3f7f09424cf1540d83d008284d6b9efb218f3e
psdcode <patrick.dorosz@gmail.com>
2019-03-16 15:58:30 -0400

    Finish testing session factory

---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,26 @@
+const Buffer = require('safe-buffer').Buffer
+const Keygrip = require('keygrip')
+const keys = require('../../config/dev')
+
+const keygrip = new Keygrip([keys.cookieKey])
+
+const sessionFactory = user => {
+  // user._id is an object containing id
+  // turn it into string to extract id
+  const session = {
+    passport: {
+      user: String(user._id),
+    },
+  }
+  const sessionStringified = JSON.stringify(session)
+  const sessionBuffered = Buffer.from(sessionStringified).toString('base64')
+
+  const sig = keygrip.sign(`session=${sessionBuffered}`)
+
+  return {
+    sig,
+    session: sessionBuffered,
+  }
+}
+
+module.exports = sessionFactory
